f0721f6568270d3c8e914cbe1a4ad96a60fd3622
millenamoll <149505940+millenamoll@users.noreply.github.com>
2024-09-25 18:12:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Grey`s Anatomy',
+    'O que é "Grey`s Anatomy"?',
+    'É uma série de drama médico criada por Shonda Rhimes, que estreou em 2005. A trama segue a vida pessoal e profissional de médicos em um hospital de Seattle.'
+)
+
+criaCartao(
+    'Grey`s Anatomy',
+    'Quem é a personagem principal da série?',
+    'A personagem principal é Meredith Grey, interpretada por Ellen Pompeo. A série começa com sua jornada como estagiária e sua evolução ao longo dos anos.'
+)
+
+criaCartao(
+    'Grey`s Anatomy',
+    'Quantas temporadas "Grey`s Anatomy" tem até agora?',
+    'A série já passou de 19 temporadas e continua em exibição, tornando-se uma das séries mais longas da televisão.'
+)
+
+criaCartao(
+    'Grey`s Anatomy',
+    'Como a série influenciou a representação de profissionais da saúde na mídia?',
+    'A série humanizou médicos, mostrando suas vulnerabilidades e desafios emocionais, o que ajudou a criar uma imagem mais realista e complexa da profissão.'
+)
